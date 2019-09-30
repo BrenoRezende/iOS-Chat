d2d96f6b058f9421618505808586c06db50eab4d
Breno Rezende <brenorrezende@gmail.com>
2019-09-30 20:26:45 -0300

    Chat improvements

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     let messageCellIdentifier = "customMessageCell"
     let messageCellNibName = "MessageCell"
+    
+    var messages = [Message]()
+    let messagesDB = Database.database().reference().child("Messages")
     
     @IBOutlet var heightConstraint: NSLayoutConstraint!
     @IBOutlet var sendButton: UIButton!
@@ -27,7 +31,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         self.navigationItem.setHidesBackButton(true, animated: false)
         heightConstraintDefaultValue = heightConstraint.constant
-        
+                
         messageTableView.delegate = self
         messageTableView.dataSource = self
         
@@ -37,21 +41,22 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.addGestureRecognizer(tapGesture)
 
         messageTableView.register(UINib(nibName: messageCellNibName, bundle: nil), forCellReuseIdentifier: messageCellIdentifier)
-                
+    
         self.configureTableView()
+        self.retrieveMessages()
     }
 
     ///////////////////////////////////////////
     
     //MARK: - TableView DataSource Methods
-    let messages = ["First Message", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", "Third Message", "Fourth Message"]
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: messageCellIdentifier, for: indexPath) as! CustomMessageCell
         
-        cell.messageBody.text = messages[indexPath.row]
+        cell.senderUsername.text = messages[indexPath.row].sender
+        cell.messageBody.text = messages[indexPath.row].body
+        cell.avatarImageView.image = #imageLiteral(resourceName: "egg")
         
         return cell
     }
@@ -91,23 +96,50 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     ///////////////////////////////////////////
     
-    
+        
     //MARK: - Send & Receive from Firebase
-    
-    
-    
-    
     
     @IBAction func sendPressed(_ sender: AnyObject) {
         
+        messageTextfield.endEditing(true)
         
-        //TODO: Send the message to Firebase and save it in our database
-        
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
+                
+        let newMessage = ["Sender" : Auth.auth().currentUser?.email, "MessageBody" : messageTextfield.text]
+            
+        self.messagesDB.childByAutoId().setValue(newMessage) {
+            (error, reference) in
+            
+            if error != nil {
+                SVProgressHUD.showError(withStatus: error?.localizedDescription)
+            } else {
+                self.messageTextfield.text = ""
+            }
+            
+            self.messageTextfield.isEnabled = true
+            self.sendButton.isEnabled = true
+        }
         
     }
     
-    //TODO: Create the retrieveMessages method here:
-    
+    func retrieveMessages() {
+        
+        self.messagesDB.observe(.childAdded) {
+            (snapshot) in
+            
+            let snapshotValue = snapshot.value as? Dictionary<String, String>
+            
+            guard let value = snapshotValue else { return }
+            guard let sender = value["Sender"] else { return }
+            guard let messageBody = value["MessageBody"] else { return }
+            
+            let message = Message(sender: sender, body: messageBody)
+            
+            self.messages.append(message)
+            self.messageTableView.reloadData()
+        }
+    }
     
 
     @IBAction func logOutPressed(_ sender: AnyObject) {
